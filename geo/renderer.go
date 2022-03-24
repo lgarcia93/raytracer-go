1fb7a90564eea2404e8bda28273b9804fd1477bb
lgarcia93 <40679264+lgarcia93@users.noreply.github.com>
2022-03-24 17:38:05 -0300

    Fixed shading problem with shading clamping

--- geo/renderer.go
+++ geo/renderer.go
@@ -25,7 +25,7 @@ func InitRenderer(outputWidth int, aspectRatio float64) *renderer {
 	}
 }
 
-func (r *renderer) Render(Origin Vec3, sphere Sphere, LightDirection Vec3) {
+func (r *renderer) Render(Origin Vec3, spheres []Sphere, LightDirection Vec3) {
 
 	img := CreateImage(r.imageWidth, r.aspectRatio)
 
@@ -37,42 +37,58 @@ func (r *renderer) Render(Origin Vec3, sphere Sphere, LightDirection Vec3) {
 
 	for i := 0; i < r.imageHeight; i++ {
 		for j := 0; j < r.imageWidth; j++ {
-
-			u := float64(j) / float64(r.imageWidth)
-			v := float64(i) / float64(r.imageHeight)
-
-			direction := lowerLeftCorner.Add(horizontalVec.MultiplyBy(u)).Add(verticalVec.MultiplyBy(v)).Subtract(Origin)
-
-			ray := Ray{
-				Origin:    Origin,
-				Direction: direction,
-			}
-
-			k := ray.Origin.Subtract(sphere.Center)
-
-			a := ray.Direction.Dot(ray.Direction)
-
-			b := 2 * k.Dot(ray.Direction)
-
-			c := k.Dot(k) - sphere.Radius*sphere.Radius
-
-			discriminant := b*b - 4.0*a*c
-
-			t1 := (-b + math.Sqrt(discriminant)) / (2.0 * a)
-
-			if discriminant >= 0 {
-				iSectPoint := ray.Origin.Add(ray.Direction.MultiplyBy(t1))
-
-				surfaceNormal := iSectPoint.Subtract(sphere.Center).Normalize()
-
-				shading := LightDirection.Normalize().Dot(surfaceNormal)
-
-				img.SetPixelColor(j, i, color.RGBA{uint8(255 * shading), uint8(255 * shading), uint8(255 * shading), 255})
-			} else {
-				img.SetPixelColor(j, i, color.RGBA{0, 0, 0, 255})
-			}
+			img.SetPixelColor(j, i, color.RGBA{0, 0, 0, 255})
 		}
 	}
 
-	img.Save("img2.png")
+	for i := 0; i < r.imageHeight; i++ {
+		for j := 0; j < r.imageWidth; j++ {
+			for _, sphere := range spheres {
+
+				u := float64(j) / float64(r.imageWidth-1)
+				v := float64(i) / float64(r.imageHeight-1)
+
+				direction := lowerLeftCorner.Add(horizontalVec.MultiplyBy(u)).Add(verticalVec.MultiplyBy(v)).Subtract(Origin)
+
+				ray := Ray{
+					Origin:    Origin,
+					Direction: direction,
+				}
+
+				k := ray.Origin.Subtract(sphere.Center)
+
+				a := ray.Direction.Dot(ray.Direction)
+
+				b := 2 * k.Dot(ray.Direction)
+
+				c := k.Dot(k) - sphere.Radius*sphere.Radius
+
+				discriminant := b*b - 4.0*a*c
+
+				t1 := (-b + math.Sqrt(discriminant)) / (2.0 * a)
+
+				if discriminant >= 0 {
+					iSectPoint := ray.Origin.Add(ray.Direction.MultiplyBy(t1))
+
+					surfaceNormal := iSectPoint.Subtract(sphere.Center).Normalize()
+
+					shading := LightDirection.Dot(surfaceNormal)
+
+					// Clamp the shading result
+					if shading < 0 {
+						shading = 0
+					}
+
+					if shading > 1 {
+						shading = 1
+					}
+
+					img.SetPixelColor(j, i, color.RGBA{uint8(255 * shading), uint8(255 * shading), uint8(255 * shading), 255})
+				}
+			}
+		}
+
+	}
+
+	img.Save("render.png")
 }
--- main.go
+++ main.go
@@ -13,13 +13,30 @@ func main() {
 		Z: 0.0,
 	}
 
-	sphere := geo.Sphere{
+	sphere1 := geo.Sphere{
 		Center: geo.Vec3{
 			X: 0.0,
 			Y: 0.0,
 			Z: -3.0,
 		},
-		Radius: 1.0,
+		Radius: 1.1,
+	}
+
+	sphere2 := geo.Sphere{
+		Center: geo.Vec3{
+			X: 0.0,
+			Y: 2.0,
+			Z: -3.0,
+		},
+		Radius: 0.9,
+	}
+	sphere3 := geo.Sphere{
+		Center: geo.Vec3{
+			X: 1.0,
+			Y: 2.0,
+			Z: -3.0,
+		},
+		Radius: 0.2,
 	}
 
 	lightDirection := geo.Vec3{
@@ -30,7 +47,11 @@ func main() {
 
 	renderer.Render(
 		origin,
-		sphere,
-		lightDirection,
+		[]geo.Sphere{
+			sphere1,
+			sphere2,
+			sphere3,
+		},
+		lightDirection.Normalize(),
 	)
 }
